1e6c5921ccf014db8a4142d3ff48b6df287ac40c
PAuloVictorSantos <paulovictor494@gmail.com>
2020-08-30 18:26:30 -0300

    validando com Bean Validation

--- src/main/java/br/com/alura/entity/AgendamentoEmail.java
+++ src/main/java/br/com/alura/entity/AgendamentoEmail.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class AgendamentoEmail {
@@ -12,7 +14,17 @@ public class AgendamentoEmail {
 	private Long id;
 	
 	@Column
+	@NotBlank(message = "{agendamentoEmail.email.vazio}")
+	@Email(message = "{agendamentoEmail.email.invalido}")
 	private String email;
+	
+	@Column
+	@NotBlank(message = "{agendamentoEmail.assunto.vazio}")
+	private String assunto;
+	
+	@Column
+	@NotBlank(message = "{agendamentoEmail.mensagem.vazio}")
+	private String mensagem;
 	
 	@Column
 	private Boolean enviado;
@@ -39,6 +51,22 @@ public class AgendamentoEmail {
 
 	public void setEnviado(Boolean enviado) {
 		this.enviado = enviado;
+	}
+
+	public String getAssunto() {
+		return assunto;
+	}
+
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
 	}
 
 	@Override
